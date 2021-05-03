d425ccdbe5cae2cf7aef743d313f34c2ab173159
root <root@localhost.localdomain>
2021-05-03 14:25:37 -0400

    menu con codigo de java no funcional mas colores de los submenus

--- js/barra.js
+++ js/barra.js
@@ -10,4 +10,27 @@ console.log(document.getElementById('sidebar'))
 
 
 
+
 });
+
+
+//submenus no funcional 
+
+$(document).ready(function(){
+
+
+    $('sidebar li:has(ul)').click(function(e){
+    e.preventDefault();
+    
+    
+    
+    if ($(this).hasClass('activado')){
+    
+    }else {
+    
+            $('sidebar li ul').slideUp();
+            $('sidebar li').removeClass('activado');
+            $(this).addClass('activado');
+            $(this).children('ul').slideDown();
+    
+    }})}
